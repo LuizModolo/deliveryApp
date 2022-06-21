3d64157c51e36941d2abce465639580a2c41fec9
Luiz Fernando C. Modolo <87335906+LuizModolo@users.noreply.github.com>
2022-06-21 11:11:34 -0300

    style: desktop style implementation on order page (seller and customer) and components

--- front-end/src/components/OrdersCard.jsx
+++ front-end/src/components/OrdersCard.jsx
@@ -2,6 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const statusMapper = {
+  Pendente: 'bg-red-500',
+  Entregue: 'bg-green-700',
+  Preparando: 'bg-yellow-500',
+  'Em Trânsito': 'bg-blue-800',
+};
+
 function OrdersCard({ orders, role }) {
   const navigate = useNavigate();
 
@@ -19,21 +26,46 @@ function OrdersCard({ orders, role }) {
   };
 
   return (
-    <div>
+    <div
+      className="flex flex-wrap justify-start mt-8"
+    >
       {orders.map(
         ({ id, deliveryAddress, deliveryNumber, saleDate, status, totalPrice }) => (
-          <button key={ id } type="button" onClick={ () => handleClick(role, id) }>
-            <p>Pedido</p>
-            <p data-testid={ `${role}_orders__element-order-id-${id}` }>
-              {id}
-            </p>
-            <p data-testid={ `${role}_orders__element-delivery-status-${id}` }>
+          <button
+            key={ id }
+            type="button"
+            onClick={ () => handleClick(role, id) }
+            className="flex flex-col justify-evenly items-center border border-transparent
+            font-medium rounded-md text-black bg-slate-300 shadow-md px-8 py-4 my-4 mx-4"
+          >
+            <div className="flex w-full justify-center mb-2">
+              <p>Pedido</p>
+              <p data-testid={ `${role}_orders__element-order-id-${id}` }>
+                :
+                {' '}
+                {id}
+              </p>
+            </div>
+            <p
+              data-testid={ `${role}_orders__element-delivery-status-${id}` }
+              className={ `mb-2 px-4 text-white rounded-md ${statusMapper[status]}` }
+            >
               {status}
             </p>
-            <p data-testid={ `${role}_orders__element-order-date-${id}` }>
+            <p
+              data-testid={ `${role}_orders__element-order-date-${id}` }
+              className="mb-2"
+            >
+              Feito em
+              {' '}
               {handleDate(saleDate)}
             </p>
-            <p data-testid={ `${role}_orders__element-card-price-${id}` }>
+            <p
+              data-testid={ `${role}_orders__element-card-price-${id}` }
+              className="text-indigo-700"
+            >
+              R$
+              {' '}
               { convertPrice(totalPrice) }
             </p>
             {
--- front-end/src/pages/CustomerOrders/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders/CustomerOrders.jsx
@@ -28,14 +28,27 @@ function CustomerOrders() {
 
   useEffect(() => {
     getAllOrders();
-  }, []);
+  }, [getAllOrders]);
 
   return (
-    <div>
+    <div className="flex flex-col bg-slate-200">
       <Header buttons={ [productsButton, ordersButton] } userName={ user.name } />
-      { !orders.status
-        ? <OrdersCard orders={ orders } role={ CUSTOMER } />
-        : <p>Nenhum produto encontrado</p> }
+      <section className="flex flex-col items-center w-full min-h-[calc(100vh-86px)]">
+        <div
+          className="flex flex-col py-16 px-4 sm:px-6 lg:px-8 w-9/12"
+        >
+          <div className="flex mb-6 justify-start bg-slate-200 md:mx-4">
+            <h2
+              className="text-2xl md:text-3xl font-medium tracking-tight text-gray-700"
+            >
+              Resumo de pedidos
+            </h2>
+          </div>
+          { !orders.status
+            ? <OrdersCard orders={ orders } role={ CUSTOMER } />
+            : <p>Nenhum produto encontrado</p> }
+        </div>
+      </section>
     </div>
   );
 }
